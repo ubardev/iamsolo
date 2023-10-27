8cc2c466b03edc3cc61196d97948417d7f6a9787
Ubar <bellin83@gmail.com>
2023-10-27 17:38:20 +0900

    :necktie: Feat: CSR -> SSG로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-'use client';
-
-import useSWR from 'swr';
 import GenerationCard from '@/components/main/GenerationCard';
 import { Generation } from '@prisma/client';
-import { useQuery } from '@tanstack/react-query';
 
-export default function Home() {
+// import useSWR from 'swr';
+// import { useQuery } from '@tanstack/react-query';
+
+export default async function Home({ params }: any) {
   // const { data: generations, isLoading, error } = useSWR('/api/generations');
 
-  const { data: generations } = useQuery<Generation[]>(
-    ['/api/generations'],
-    () => fetch('/api/generations').then((res) => res.json()),
-    { select: (data) => data },
-  );
+  // const { data: generations } = useQuery<Generation[]>(
+  //   ['/api/generations'],
+  //   () => fetch('/api/generations').then((res) => res.json()),
+  //   { select: (data) => data },
+  // );
+
+  const generations = await fetch('http://127.0.0.1:3000/api/generations', {
+    cache: 'force-cache',
+  }).then((res) => res.json());
 
   return (
-    // <div className="p-6 flex justify-center gap-2 flex-wrap">
     <div className="grid grid-cols-2 gap-4">
       {generations &&
         generations.map((generation: Generation) => (
@@ -23,4 +25,14 @@ export default function Home() {
         ))}
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  const generations = await fetch('http://127.0.0.1:3000/api/generations', {
+    cache: 'force-cache',
+  }).then((res) => res.json());
+
+  return generations.map((generation: Generation) => ({
+    id: generation.id.toString(),
+  }));
 }
