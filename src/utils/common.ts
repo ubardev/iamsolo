37a7995bd64cdeacb388785a2494b33bde4c758b
Ubar <bellin83@gmail.com>
2023-10-29 02:42:13 +0900

    :necktie: Feat: 추가정보 노출 추가

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,13 +1,14 @@
 import { Member } from '@prisma/client';
 
 export const getMemberTags = (member: Member) => {
-  const { yearOfBirth, job, school, address, car } = member;
+  const { yearOfBirth, job, school, address, car, moreInfo } = member;
   let result: String[] = [`${yearOfBirth}년생` || ''];
 
   setItems(result, job);
   setItems(result, school);
   setItems(result, address);
   setItems(result, car);
+  setItems(result, moreInfo);
 
   return result;
 };
