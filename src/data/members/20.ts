76cc6a5f7343e3699c25b453639de6abf12a6451
austin <austin.hwang@kakaoenterprise.com>
2024-04-27 00:06:55 +0900

    :necktie: 20기 남자 출연자 정보 추가

--- src/data/members/20.ts
+++ src/data/members/20.ts
@@ -28,5 +28,70 @@ export const member_20 = {
             "dislikeCount": 0,
             "sequence": 1
         },
+        {
+            "id": 125,
+            "gender": "male",
+            "name": "영호",
+            "yearOfBirth": null,
+            "job": "현대 엔지니어링(?)",
+            "school": null,
+            "address": null,
+            "car": "그랜저HG",
+            "moreInfo": null,
+            "image": "/images/member/20/20기-영호.webp",
+            "instgramUrl": null,
+            "youtubeUrl": null,
+            "shopUrl": null,
+            "generationId": 20,
+            "likeCount": 0,
+            "dislikeCount": 0,
+            "sequence": 2
+        },
+        {
+            "id": 126,
+            "gender": "male",
+            "name": "영식",
+            "yearOfBirth": null,
+            "job": "농협 은행(?)",
+            "school": null,
+            "address": null,
+            "car": "기아 K7",
+            "moreInfo": null,
+            "image": "/images/member/20/20기-영식.webp",
+            "instgramUrl": null,
+            "youtubeUrl": null,
+            "shopUrl": null,
+            "generationId": 20,
+            "likeCount": 0,
+            "dislikeCount": 0,
+            "sequence": 3
+        },
+        {
+            "id": 127,
+            "gender": "male",
+            "name": "영철",
+            "yearOfBirth": null,
+            "job": "인텔 반도체 엔지니어",
+            "school": null,
+            "address": "미국 오리건주 거주",
+            "car": "기아 스포티지",
+            "moreInfo": null,
+            "image": "/images/member/20/20기-영철.webp",
+            "instgramUrl": null,
+            "youtubeUrl": null,
+            "shopUrl": null,
+            "generationId": 20,
+            "likeCount": 0,
+            "dislikeCount": 0,
+            "sequence": 4
+        },
     ]
 }
+
+
+// - 광수 : 카이스트 연구원(?), 서울 과학고, 연세대
+// - 상철 : BMW 3시리즈, 광주 고향, 삼성전자(?)
+//
+// - 영숙 : 르노 SM5, 대원외고
+// - 정숙 : 벤츠 C200 쿠페
+// - 순자 : 쉐보레 트렉스 크로스오버, 진주 고향, 김해외고졸업, 서울 강동구 자가 보유
